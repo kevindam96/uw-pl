d00e7a00787f413dd675048e07933bb778d7aa31
kevindam96 <kevindam96@gmail.com>
2019-12-08 21:02:36 -0600

    correct hw6

--- Week 8/hw6assignment.rb
+++ Week 8/hw6assignment.rb
@@ -5,18 +5,10 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
-    All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
-                    rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
-                    [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
-                    [[0, 0], [0, -1], [0, 1], [0, 2]]],
-                    rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-                    rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
-                    rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-                    rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
-                    rotations([[0, 0], [-1, 0], [-1, 1], [0, 1], [1, 0]]), # thumbs-up
-                    [[[0, 0], [-1, 0], [1, 0], [-2, 0], [2, 0]], # longer (only needs two)
-                    [[0, 0], [0, -1], [0, 1], [0, -2], [0, 2]]],
-                    rotations([[0, 0], [0, 1], [1, 0], [0, 0]])] # heart
+    All_My_Pieces = All_Pieces + [rotations([[0, 0], [-1, 0], [-1, 1], [0, 1], [1, 1]]), # thumbs-up
+                                 [[[0, 0], [-1, 0], [1, 0], [-2, 0], [2, 0]], # longer (only needs two)
+                                 [[0, 0], [0, -1], [0, 1], [0, -2], [0, 2]]],
+                                 rotations([[0, 0], [0, 1], [1, 0]])] # heart
 
   # your enhancements here
   def self.next_piece (board)
@@ -24,7 +16,7 @@ class MyPiece < Piece
   end
 
   def self.cheat_piece (board)
-    MyPiece.new([[[0, 0], [0, 0], [0, 0], [0, 0]]], board)
+    MyPiece.new([[[0, 0]]], board)
   end
 end
 
@@ -54,13 +46,25 @@ class MyBoard < Board
   def next_piece
     if @cheat_flag then
       @current_block = MyPiece.cheat_piece(self)
-      @current_pos = nil
       @cheat_flag = false
     else
       @current_block = MyPiece.next_piece(self)
-      @current_pos = nil
     end
+    @current_pos = nil
   end
+
+  def store_current
+    locations = @current_block.current_rotation
+    displacement = @current_block.position
+    (0..(locations.size - 1)).each{|index| 
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
+  end
+
 
   def cheat
     if @score >= 100 && !@cheat_flag then
@@ -92,28 +96,8 @@ class MyTetris < Tetris
 
 
   def key_bindings  
-    @root.bind('n', proc {self.new_game}) 
-
-    @root.bind('p', proc {self.pause}) 
-
-    @root.bind('q', proc {exitProgram})
-    
-    @root.bind('a', proc {@board.move_left})
-    @root.bind('Left', proc {@board.move_left}) 
-    
-    @root.bind('d', proc {@board.move_right})
-    @root.bind('Right', proc {@board.move_right}) 
-
-    @root.bind('s', proc {@board.rotate_clockwise})
-    @root.bind('Down', proc {@board.rotate_clockwise})
-
-    @root.bind('w', proc {@board.rotate_counter_clockwise})
-    @root.bind('Up', proc {@board.rotate_counter_clockwise}) 
-
+    super
     @root.bind('u', proc {@board.rotate_180})
-
-    @root.bind('space', proc {@board.drop_all_the_way}) 
-
     @root.bind('c', proc {@board.cheat})
   end
   
